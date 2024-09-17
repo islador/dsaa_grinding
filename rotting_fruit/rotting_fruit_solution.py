86989dcaa244dcb0b335fed3dc9f1c747ee18329
Luke Mercado <lukemercado@lukes-mbp.local.meter>
2024-09-17 12:59:25 -0700

    Removed the extra fruit object, cleaned up a couple of the names for readability.

--- rotting_fruit/rotting_fruit_solution.py
+++ rotting_fruit/rotting_fruit_solution.py
@@ -1,10 +1,3 @@
-class fruit:
-    def __init__(self, position):
-        self.position = position
-        self.rotten = False
-        self.fresh = True
-        self.distance_to_rotten = 0
-
 class Solution:
     @classmethod
     def is_new_position_the_previous_position(self, new_position, previous_position):
@@ -26,24 +19,24 @@ class Solution:
         return False
 
     @classmethod
-    def storage_array_index_from_position(self, column_count, position):
+    def rot_propagation_by_position_index_from_position(self, column_count, position):
         return (position[0] * column_count) + position[1]
 
     @classmethod
     # Computational complexity is, at worst, (R x C)x T where R is Row count, C is column count and T is the number of zeros identified in the matrix.
-    def evaluate_position(self, grid, row_count, column_count, new_position, previous_position, distance_traversed, storage_array):
+    def evaluate_position_against_rules(self, grid, row_count, column_count, new_position, previous_position, distance_traversed, rot_propagation_by_position):
         # If new position is not off the edge
         if self.is_new_position_in_grid(row_count, column_count, new_position) == False:
             # If new position is not the previous position
             if self.is_new_position_the_previous_position(new_position, previous_position) == False:
                 if grid[new_position[0]][new_position[1]] == 1:
                     # Check the value currently assigned to it:
-                    new_position_current_propagation_distance = storage_array[self.storage_array_index_from_position(column_count, new_position)]
+                    new_position_current_propagation_distance = rot_propagation_by_position[self.rot_propagation_by_position_index_from_position(column_count, new_position)]
                     if new_position_current_propagation_distance == 0:
-                        #ab. and its current storage_array value is zero -> update the storage_array with the value, and recurse
+                        #ab. and its current rot_propagation_by_position value is zero -> update the rot_propagation_by_position with the value, and recurse
                         return True
                     if new_position_current_propagation_distance >= distance_traversed +1:
-                        #ac. or its current storage_array value is the same or higher than the current distance traversed +1 -> update the storage_array with the value, and recurse
+                        #ac. or its current rot_propagation_by_position value is the same or higher than the current distance traversed +1 -> update the rot_propagation_by_position with the value, and recurse
                         return True
         return False
     
@@ -54,70 +47,71 @@ class Solution:
         current_column = 0
         fresh_fruit_count = 0
         rotten_fruits = []
-        storage_array = []
+        rot_propagation_by_position = [] # This is a flattened array to allow for easy completion tracking.
         while current_row < row_count:
             while current_column < column_count:
                 if grid[current_row][current_column] == 2:
                     rotten_fruits.append([current_row, current_column])
                 if grid[current_row][current_column] == 1:
                     fresh_fruit_count+=1
-                storage_array.append(0)
+                rot_propagation_by_position.append(0)
                 current_column+=1
             current_row+=1
             current_column = 0
-        return rotten_fruits, storage_array, fresh_fruit_count
+        return rotten_fruits, rot_propagation_by_position, fresh_fruit_count
 
     @classmethod
-    def propagate_rot(self, row_count, column_count, grid, current_position, previous_position, distance_traversed, storage_array):
+    def propagate_rot(self, row_count, column_count, grid, current_position, previous_position, distance_traversed, rot_propagation_by_position):
         current_value = grid[current_position[0]][current_position[1]]
-        # Check adjacent squares
+        # Check adjacent squares in sequence
         # Check Left
         new_position = [current_position[0], current_position[1]-1]
-        if self.evaluate_position(grid, row_count, column_count, new_position, previous_position, distance_traversed, storage_array) == True: 
-            # Set the value of the storage_array for this index to distance_traversed + 1
-            storage_array[self.storage_array_index_from_position(column_count, new_position)] = distance_traversed + 1
+        if self.evaluate_position_against_rules(grid, row_count, column_count, new_position, previous_position, distance_traversed, rot_propagation_by_position) == True: 
+            # Set the value of the rot_propagation_by_position for this index to distance_traversed + 1
+            rot_propagation_by_position[self.rot_propagation_by_position_index_from_position(column_count, new_position)] = distance_traversed + 1
             # Recurse
-            self.propagate_rot(row_count, column_count, grid, new_position, current_position, distance_traversed+1, storage_array)
+            self.propagate_rot(row_count, column_count, grid, new_position, current_position, distance_traversed+1, rot_propagation_by_position)
         
         # Check Up
         new_position = [current_position[0]-1, current_position[1]]
         # If new position is not off the edge
-        if self.evaluate_position(grid, row_count, column_count, new_position, previous_position, distance_traversed, storage_array) == True: 
-            # Set the value of the storage_array for this index to distance_traversed + 1
-            storage_array[self.storage_array_index_from_position(column_count, new_position)] = distance_traversed + 1
+        if self.evaluate_position_against_rules(grid, row_count, column_count, new_position, previous_position, distance_traversed, rot_propagation_by_position) == True: 
+            # Set the value of the rot_propagation_by_position for this index to distance_traversed + 1
+            rot_propagation_by_position[self.rot_propagation_by_position_index_from_position(column_count, new_position)] = distance_traversed + 1
             # Recurse
-            self.propagate_rot(row_count, column_count, grid, new_position, current_position, distance_traversed+1, storage_array)
+            self.propagate_rot(row_count, column_count, grid, new_position, current_position, distance_traversed+1, rot_propagation_by_position)
 
         # Check Right
         new_position = [current_position[0], current_position[1]+1]
-        if self.evaluate_position(grid, row_count, column_count, new_position, previous_position, distance_traversed, storage_array) == True: 
-            # Set the value of the storage_array for this index to distance_traversed + 1
-            storage_array[self.storage_array_index_from_position(column_count, new_position)] = distance_traversed + 1
+        if self.evaluate_position_against_rules(grid, row_count, column_count, new_position, previous_position, distance_traversed, rot_propagation_by_position) == True: 
+            # Set the value of the rot_propagation_by_position for this index to distance_traversed + 1
+            rot_propagation_by_position[self.rot_propagation_by_position_index_from_position(column_count, new_position)] = distance_traversed + 1
             # Recurse
-            self.propagate_rot(row_count, column_count, grid, new_position, current_position, distance_traversed+1, storage_array)
+            self.propagate_rot(row_count, column_count, grid, new_position, current_position, distance_traversed+1, rot_propagation_by_position)
         
         # Check Down
         new_position = [current_position[0]+1, current_position[1]]
-        if self.evaluate_position(grid, row_count, column_count, new_position, previous_position, distance_traversed, storage_array) == True: 
-            # Set the value of the storage_array for this index to distance_traversed + 1
-            storage_array[self.storage_array_index_from_position(column_count, new_position)] = distance_traversed + 1
+        if self.evaluate_position_against_rules(grid, row_count, column_count, new_position, previous_position, distance_traversed, rot_propagation_by_position) == True: 
+            # Set the value of the rot_propagation_by_position for this index to distance_traversed + 1
+            rot_propagation_by_position[self.rot_propagation_by_position_index_from_position(column_count, new_position)] = distance_traversed + 1
             # Recurse
-            self.propagate_rot(row_count, column_count, grid, new_position, current_position, distance_traversed+1, storage_array)
+            self.propagate_rot(row_count, column_count, grid, new_position, current_position, distance_traversed+1, rot_propagation_by_position)
 
     def orangesRotting(self, grid: List[List[int]]) -> int:
         row_count = len(grid)
         column_count = len(grid[0])
-        rotten_fruits, storage_array, fresh_fruit_count = self.find_rotten_fruits_and_count_fresh_fruits(row_count, column_count, grid)
+        rotten_fruits, rot_propagation_by_position, fresh_fruit_count = self.find_rotten_fruits_and_count_fresh_fruits(row_count, column_count, grid)
         for index in rotten_fruits:
-            self.propagate_rot(row_count, column_count, grid, index, index, 0, storage_array)
-        max_rot_propagation = 0
+            self.propagate_rot(row_count, column_count, grid, index, index, 0, rot_propagation_by_position)
+        
+        max_rot_propagation_time = 0
         rotted_fruit_count = 0
-        for value in storage_array:
+        for value in rot_propagation_by_position:
             if value > 0:
                 rotted_fruit_count+=1
-            if value > max_rot_propagation:
-                max_rot_propagation = value
+            if value > max_rot_propagation_time:
+                max_rot_propagation_time = value
         if rotted_fruit_count == fresh_fruit_count:
-            return max_rot_propagation
+            return max_rot_propagation_time
         else:
             return -1
