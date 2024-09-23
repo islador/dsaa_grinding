a14b8d7f144404126a5bf07239325e18dbc9548d
Luke Mercado <luke.isla@gmail.com>
2024-09-23 10:26:54 -0700

    Created files for a V2 attempt, this will be recursive.

---
+++ pacific_atlantic_water_flow/pacific_atlantic_water_flow_v2.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        # Configuration Variables
+        row_count = len(heights)
+        column_count = len(heights[0])
+
+input_array = [[2,2,2],[2,1,2],[2,2,2]]
+print(f"{Solution.pacificAtlantic(Solution, input_array)}")
