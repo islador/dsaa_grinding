1da07ede489bbaae680a6e40caa1e4c9c3c0cb54
Luke Mercado <luke.isla@gmail.com>
2024-09-24 12:51:57 -0700

    Added build_flow_destinations_matrix to build out a matrix of the same size as the input matrix and populate it with known destination information. This matrix will be used to allow us to easily track the values stored. A more memory efficient mechanism might be possible, but for now functionality.

--- pacific_atlantic_water_flow/pacific_atlantic_water_flow_v2.py
+++ pacific_atlantic_water_flow/pacific_atlantic_water_flow_v2.py
@@ -8,7 +8,6 @@ class Solution:
 
     @classmethod
     def stable_sort_matrix(self, row_count, column_count, matrix):
-        # Load the matrix
         current_row = 0
         current_column = 0
         values_in_order = []
@@ -19,6 +18,8 @@ class Solution:
                 current_column+=1
             current_row +=1
             current_column = 0
+
+        # Bubble Sort the built array to achieve a stable lowest to highest value sort
         for n in range(len(values_in_order)-1, 0, -1):
             for i in range(n):
                 if values_in_order[i].value > values_in_order[i+1].value:
@@ -26,14 +27,37 @@ class Solution:
                     values_in_order[i], values_in_order[i+1] = values_in_order[i+1], values_in_order[i]
         return values_in_order
 
+    @classmethod
+    def build_flow_destinations_matrix(self, row_count, column_count):
+        current_row = 0
+        current_column = 0
+        flow_destinations = []
+        # Expand the matrix and populate it with known flow destinations
+        while current_row < row_count:
+            flow_destinations.append([])
+            while current_column < column_count:
+                flow_destinations[current_row].append([])
+                if (current_row == 0) and (current_column == column_count-1):
+                    flow_destinations[current_row][current_column] = {"Pacific": True, "Atlantic": True}
+                elif (current_row == row_count-1) and (current_column == 0):
+                    flow_destinations[current_row][current_column] = {"Pacific": True, "Atlantic": True}
+                else:
+                    flow_destinations[current_row][current_column] = {}
+                current_column+=1
+            current_row +=1
+            current_column = 0
+        return flow_destinations
 
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         # Configuration Variables
         row_count = len(heights)
         column_count = len(heights[0])
         sorted_coordinates = self.stable_sort_matrix(row_count, column_count, heights)
-        for cell in sorted_coordinates:
-            print(f"{cell.coordinates}")
+        flow_destinations = self.build_flow_destinations_matrix(row_count, column_count)
+        print(f"Flow Destinations")
+        print(f"{flow_destinations}")
+        #for cell in sorted_coordinates:
+        #    print(f"{cell.coordinates}")
         return [[1]]
 
 #input_array = [[2,2,2],[2,1,2],[2,2,2]]
