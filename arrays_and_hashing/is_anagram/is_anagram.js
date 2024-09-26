56e03808737538c8340c18a140abf42cb9adef2f
Luke Mercado <luke.isla@gmail.com>
2024-09-26 11:20:29 -0700

    Wrote an efficient solution to look for a reversed word. Sadly I misunderstood what anagram meant, and this is useless. Onwards to v2.

--- arrays_and_hashing/is_anagram/is_anagram.js
+++ arrays_and_hashing/is_anagram/is_anagram.js
@@ -6,6 +6,14 @@ class Solution {
      */
     isAnagram(s, t) {
         // Sliding pointers?
-        
+        for(let i = 0; i < s.length; i++) {
+            let s_index = i
+            let t_index = (t.length - i)-1
+            
+            console.log("t_index: ", t_index, "s_index: ", s_index)
+            console.log("t at index: ", t.at(t_index), "s at index: ", s.at(s_index))
+            if(s.at(s_index) != t.at(t_index)) { return false}
+        }
+        return true
     }
 }
