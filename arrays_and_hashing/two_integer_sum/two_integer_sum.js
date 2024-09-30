694e9fa753f0abf0cc46a8e775b554734963ed75
Luke Mercado <luke.isla@gmail.com>
2024-09-30 13:37:29 -0700

    Left over final commit from last week... sad panda.

--- arrays_and_hashing/two_integer_sum/two_integer_sum.js
+++ arrays_and_hashing/two_integer_sum/two_integer_sum.js
@@ -10,20 +10,18 @@ class Solution {
         for(let i = 0; i < nums.length; i++) {
             let inputNumber = nums[i]
             const soughtAddend = target - inputNumber
-            if(soughtAddend != inputNumber) {
-                if(soughtAddends.has(inputNumber)) {
-                    return [soughtAddends.get(inputNumber), i]
-                }
-                if(seenNumbers.has(soughtAddend)) {
-                    return [seenNumbers.get(soughtAddend), i]
-                }
-                // I want to preserve the lowest index value of the seen and sought numbers, so I can't just overwrite them.
-                if(soughtAddends.has(soughtAddend) == false) {
-                    soughtAddends.set(soughtAddend, i)
-                }
-                if(seenNumbers.has(inputNumber) == false) {
-                    seenNumbers.set(inputNumber, i)
-                }
+            if(soughtAddends.has(inputNumber)) {
+                return [soughtAddends.get(inputNumber), i]
+            }
+            if(seenNumbers.has(soughtAddend)) {
+                return [seenNumbers.get(soughtAddend), i]
+            }
+            // I want to preserve the lowest index value of the seen and sought numbers, so I can't just overwrite them.
+            if(soughtAddends.has(soughtAddend) == false) {
+                soughtAddends.set(soughtAddend, i)
+            }
+            if(seenNumbers.has(inputNumber) == false) {
+                seenNumbers.set(inputNumber, i)
             }
         }
 
