63aab36d67af263c591a809672210e2d1af72f19
Luke Mercado <luke.isla@gmail.com>
2024-09-30 19:42:18 -0700

    Implemented the solution's algorithm. I'm not particularly pleased with it, it does what it should, and seems to be as performant as expected, but is just downright ugly. I'm going to need to figure out what good JS looks like.

--- arrays_and_hashing/anagram_groups/anagram_groups.js
+++ arrays_and_hashing/anagram_groups/anagram_groups.js
@@ -3,5 +3,37 @@ class Solution {
      * @param {string[]} strs
      * @return {string[][]}
      */
-    groupAnagrams(strs) {}
+    groupAnagrams(strs) {
+        let knownAnagrams = new Map()
+        for(let string of strs){
+            // Assemble a count of all the letters
+            let letterHash = new Map()
+            let sortedString = string.split("").sort().join("")
+            for (let character of sortedString) {
+                if (letterHash.get(character) != undefined) {
+                    letterHash.set(character, letterHash.get(character) +1) 
+                } else {
+                    letterHash.set(character, 1)
+                }
+            }
+            // Turn the map into a lookup key
+            let lookupKey = ""
+            letterHash.forEach((values, keys) => {
+                lookupKey = lookupKey.concat(keys,values)
+            })
+            // Group anagrams
+            if (knownAnagrams.get(lookupKey) != undefined) {
+                let answerList = knownAnagrams.get(lookupKey)
+                answerList.push(string)
+                knownAnagrams.set(lookupKey, answerList) 
+            } else {
+                knownAnagrams.set(lookupKey, [string])
+            }
+        }
+        let results = []
+        knownAnagrams.forEach((values, keys) => {
+            results.push(values)
+        })
+        return results
+    }
 }
