3e945000ffde14bf533d6771f143ce19cbac0324
Luke Mercado <luke.isla@gmail.com>
2024-10-02 12:32:45 -0700

    Began working through some edge cases and starting to engage in detail, but realized that I need a faster iteration loop. Writing some basic scenarios does fine, but I also have a desire to learn the test setup (both in VSCode and in JS land in general) and these scenarios feel a lot like a good test harness would rapidly speed things up. So lets go looking at test harnesses I can wrap around this.

--- arrays_and_hashing/string_encode_and_decode/string_encode_and_decode.js
+++ arrays_and_hashing/string_encode_and_decode/string_encode_and_decode.js
@@ -3,11 +3,44 @@ class Solution {
    * @param {string[]} strs
    * @returns {string}
    */
-  encode(strs) {}
+  encode(strs) {
+    let encodedString = ""
+    for(let str of strs) {
+
+    }
+    return encodedString
+  }
 
   /**
    * @param {string} str
    * @returns {string[]}
    */
-  decode(str) {}
+  decode(str) {
+    if (str.length > 0){
+      return str.split("//")
+    } else {
+      return []
+    }
+  }
 }
+
+let solution = new Solution()
+let encoded = solution.encode(["cat","dog","","mouse"])
+console.log("Encoded: ", encoded)
+console.log("Decoded: ", solution.decode(encoded))
+
+let encoded2 = solution.encode(["\/\/","^.02@","meow","|\|\|"])
+console.log("Encoded: ", encoded2)
+console.log("Decoded: ", solution.decode(encoded2))
+
+
+console.log("One Empty String")
+let encoded3 = solution.encode([""])
+console.log("Encoded: ", encoded3)
+console.log("Decoded: ", solution.decode(encoded3))
+
+
+console.log("Empty Array")
+let encoded4 = solution.encode([])
+console.log("Encoded: ", encoded4)
+console.log("Decoded: ", solution.decode(encoded4))
