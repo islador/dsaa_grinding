7d3af8c13809b872fc9f085feed144939ebc39ba
Luke Mercado <luke.isla@gmail.com>
2024-10-03 12:43:56 -0700

    Expanded and greened up my tests. Sadly I dont have emojis covered, so this isn't yet workable. Next step is to write out a test case that covers emoji input (which breaks down my character counting IIRC).

--- arrays_and_hashing/string_encode_and_decode/string_encode_and_decode.js
+++ arrays_and_hashing/string_encode_and_decode/string_encode_and_decode.js
@@ -7,13 +7,9 @@ module.exports = class Solution {
     if(strs.length == 0) { return undefined }
     let encodedString = ""
 
-    for(let i = 0; i < strs.length; i++) {
-      encodedString = encodedString.concat('\"',strs[i],'\"')
-      if(i < (strs.length - 1)){
-        encodedString = encodedString.concat(',')
-      }
+    for(let inputString of strs) {
+      encodedString = encodedString.concat(inputString.length).concat("@)").concat(inputString)
     }
-    //console.log("encodedString: ", encodedString)
     return encodedString
   }
 
@@ -24,10 +20,68 @@ module.exports = class Solution {
   decode(str) {
     if(str == undefined) { return [] }
     console.log("str: ", str)
-    if (str.length > 0){
-      return str.split('","')
-    } else {
-      return []
+    let results = []
+    let readingEncodingInput = true
+    let falseReadingEncodingInputNextIteration = false
+    let atEncountered = false
+    let closeParenEncountered = false
+    let delimitedString = ""
+
+    let remainingCharactersInEncodedString = 0
+    let encodedString = ""    
+    
+    for(let character of str) {
+      console.log("character: ", character)
+      // Wait an iteration and reset everything
+      if(falseReadingEncodingInputNextIteration == true){
+        console.log("resetting")
+        readingEncodingInput = false
+        atEncountered = false
+        closeParenEncountered = false
+        delimitedString = ""
+      }
+      // Ingest and process the encoded input if that is where the cursor is
+      if(readingEncodingInput == true) {
+        console.log("building delimitedString")
+        delimitedString = delimitedString.concat(character)
+        if(character == "@") {
+          atEncountered = true
+        }
+        if(atEncountered == true) {
+          if(character == ")") {
+            closeParenEncountered = true
+          }
+        }
+        if(atEncountered == true && closeParenEncountered == true) {
+          remainingCharactersInEncodedString = parseInt(delimitedString.split("@")[0])
+          console.log("setting remainingCharactersInEncodedString:", remainingCharactersInEncodedString)
+
+          if(remainingCharactersInEncodedString == 0){
+            results.push("")
+            console.log("Pushing zero string")
+            atEncountered = false
+            closeParenEncountered = false
+            delimitedString = ""
+          }else{
+            falseReadingEncodingInputNextIteration = true
+          }
+        }
+      }
+      //
+      if(readingEncodingInput == false) {
+        console.log("remainingCharactersInEncodedString: ", remainingCharactersInEncodedString)
+        encodedString = encodedString.concat(character)
+        if(remainingCharactersInEncodedString == 1) {
+          console.log("encodedString:", encodedString)
+          readingEncodingInput = true
+          falseReadingEncodingInputNextIteration = false
+          results.push(encodedString)
+          encodedString = ""
+        }
+        remainingCharactersInEncodedString -=1
+      }
     }
+    console.log("results Length: ", results.length)
+    return results
   }
 }
--- arrays_and_hashing/string_encode_and_decode/string_encode_and_decode.test.js
+++ arrays_and_hashing/string_encode_and_decode/string_encode_and_decode.test.js
@@ -2,35 +2,55 @@ const Solution = require('./string_encode_and_decode');
 const solution = new Solution()
 describe('Encode', () => {
 
-  test.skip('An empty array outputs undefined', () => {
+  test('An empty array outputs undefined', () => {
     let param = []
     expect(solution.encode(param)).toBe(undefined)
   });
 
-  test.skip('An array with an empty string outputs an escaped string', () => {
+  test('An array with an empty string outputs a string of "0@)"', () => {
     let param = [""]
-    expect(solution.encode(param)).toEqual("\"\"")
+    expect(solution.encode(param)).toEqual("0@)")
   });
 
-  test.skip('An array with three strings outputs a string with multiple escapes', () => {
+  test('An array with three strings outputs a string following our delimiter logic', () => {
     let param = ["cat","","dog"]
-    expect(solution.encode(param)).toEqual("\"cat\",\"\",\"dog\"")
+    expect(solution.encode(param)).toEqual("3@)cat0@)3@)dog")
+  });
+
+  test('An array with a string containing our delimiter still outputs the correct character counts', () => {
+    let param = ["cat","2@)","dog"]
+    expect(solution.encode(param)).toEqual("3@)cat3@)2@)3@)dog")
+  });
+
+  test('An array with a double digit length string encodes a double digit number', () => {
+    let param = ["cat","1234567890","dog"]
+    expect(solution.encode(param)).toEqual("3@)cat10@)12345678903@)dog")
   });
 })
 
 describe('Decode', () => {
-  test.skip('An undefined input outputs an empty array', () => {
+  test('An undefined input outputs an empty array', () => {
     let param = undefined
     expect(solution.decode(param)).toEqual([])
   });
 
   test('An empty string input outputs an array with an empty string', () => {
-    let param = '""'
+    let param = "0@)"
     expect(solution.decode(param)).toEqual([""])
   });
 
-  test.skip('An escaped string outputs the pre-encoding array', () => {
-    let param = "\"cat\",\"\",\"dog\""
+  test('An encoded string outputs the pre-encoding array', () => {
+    let param = "3@)cat2@)it3@)dog"
+    expect(solution.decode(param)).toEqual(["cat","it","dog"])
+  });
+
+  test('An encoded string with a zero length string inside outputs the pre-encoding array', () => {
+    let param = "3@)cat0@)3@)dog"
     expect(solution.decode(param)).toEqual(["cat","","dog"])
+  });
+
+  test('An encoded string with our delimiter scheme as an input outputs the pre-encoding array correctly', () => {
+    let param = "3@)cat3@)2@)3@)dog"
+    expect(solution.decode(param)).toEqual(["cat","2@)","dog"])
   });
 })
